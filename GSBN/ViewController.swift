ddc4687cabc086fe13d8155cc3c98f76cdebcd76
Dae In Lee <lazy0ren@gmail.com>
2018-05-10 16:46:46 +0900

    Empty Single View Project

---
+++ GSBN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GSBN
+//
+//  Created by Lazy Ren on 10/05/2018.
+//  Copyright © 2018 Lazy Ren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
