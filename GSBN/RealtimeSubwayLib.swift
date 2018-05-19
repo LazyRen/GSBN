b2e54562c7fae8c84731a4eac7e0b9275e2f0f1f
서창범 <seochangbeom@seochangbeom-ui-MacBook-Pro.local>
2018-05-19 15:59:01 +0900

    add realtimeSubwayPositionListEntry struct
    한 열차의 현재 위치와 운행 상태 등을 저장하는 object를 추가함.

--- GSBN/RealtimeSubwayLib.swift
+++ GSBN/RealtimeSubwayLib.swift
@@ -11,6 +11,76 @@ import Foundation
 //test api url
 let urlstr = "http://swopenAPI.seoul.go.kr/api/subway/6e574a4d58636b6436357942596163/json/realtimePosition/0/50/2호선"
 
+struct realtimeSubwayPositionListEntry {
+    var trainNo : Int = 0       // 2300       // 열차번호
+    var statnTid : Int = 0      // 1002000211 // 종착지하철역아이디
+    var statnNm : String = ""   // 합정        // 지하철역이름
+    var trainSttus : Int = 0    // 2          // 0 : 진입, 1 : 도착, 2 : 그외 상태
+    var directAt : Int = 0      // 0          // 급행여부
+    var lastRecptnDt : Int = 0  // 20180518   // 최종수신날짜
+    var statnTnm : String = ""  // 성수종착     // 종착역이름
+    var subwayId : Int = 0      // 1002       // 지하철 호선 ID
+    var updnLine : Int = 0      // 0          // 0 : 상행(외선) 1 : 하행(내선)
+    var lstcarAt : Int = 0      // 0          // 막차여부
+    var recptnDt : String = ""  // 2018-05-18 16:47:55      //당장은 스트링으로 하는데 추후 타임스탬프로 구현. 그래야 시간 연산 용이. 수신된 시간
+    var statnId : Int = 0       // 1002000238 //지하철 역 아이디
+    var subwayNm : String = ""  // 2호선       //지하철 호선명
+    
+    init (parsedData : [String:Any]) {
+        if let fetchedtrainNo = parsedData["trainNo"] as? Int {
+            self.trainNo = fetchedtrainNo
+        }
+        
+        if let fetchedstatnTid = parsedData["statnTid"] as? Int {
+            self.statnTid = fetchedstatnTid
+        }
+        
+        if let fetchedstatnNm = parsedData["statnNm"] as? String {
+            self.statnNm = fetchedstatnNm
+        }
+        
+        if let fetchedtrainSttus = parsedData["trainSttus"] as? Int {
+            self.trainSttus = fetchedtrainSttus
+        }
+        
+        if let fetchedtrainSttus = parsedData["directAt"] as? Int {
+            self.directAt = fetchedtrainSttus
+        }
+        
+        if let fetchedlastRecptnDt = parsedData["lastRecptnDt"] as? Int {
+            self.lastRecptnDt = fetchedlastRecptnDt
+        }
+        
+        if let fetchedstatnTnm = parsedData["statnTnm"] as? String {
+            self.statnTnm = fetchedstatnTnm
+        }
+        
+        if let fetchedsubwayId = parsedData["subwayId"] as? Int {
+            self.subwayId = fetchedsubwayId
+        }
+        
+        if let fetchedupdnLine = parsedData["updnLine"] as? Int {
+            self.updnLine = fetchedupdnLine
+        }
+        
+        if let fetchedlstcarAt = parsedData["lstcarAt"] as? Int {
+            self.lstcarAt = fetchedlstcarAt
+        }
+        
+        if let fetchedrecptnDt = parsedData["recptnDt"] as? String {  //당장은 스트링으로 하는데 추후 타임스탬프로 구현. 그래야 시간 연산 용이
+            self.recptnDt = fetchedrecptnDt
+        }
+        
+        if let fetchedstatnId = parsedData["statnId"] as? Int {
+            self.statnId = fetchedstatnId
+        }
+        
+        if let fetchedsubwayNm = parsedData["subwayNm"] as? String {
+            self.subwayNm = fetchedsubwayNm
+        }
+    }
+}
+
 func getRealtimeSubwayPosition () -> Void {
     
     //URL에 한글이 포함되어 URL인코딩을 해야한다.
