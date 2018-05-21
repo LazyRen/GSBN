f8aa9f3fac25d0af6fe38e3905babb4f8a5ddabc
Dae In Lee <lazy0ren@gmail.com>
2018-05-22 05:02:24 +0900

    GPS permission & tracking my location

--- GSBN/ViewController.swift
+++ GSBN/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var myMapKitView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        myMapKitView.delegate = self
+        myMapKitView.showsPointsOfInterest = true
+        myMapKitView.showsUserLocation = true
+        
+        func enableLocationServices() {
+            locationManager.delegate = self
+            switch CLLocationManager.authorizationStatus() {
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+                break;
+            default:
+                break;
+            }
+        }
+        enableLocationServices()
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.startUpdatingLocation()
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    func locationManager(_ _manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first!
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 500, 500)
+        myMapKitView.setRegion(coordinateRegion, animated: true)
+        locationManager.stopUpdatingLocation()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
